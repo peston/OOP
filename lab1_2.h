294c5d6d2164c271d8fd5f7de48ad1d7b760d9b4
Gavrilov Andrey <gavrilovand@mail.ru>
2013-10-03 14:37:34 +0600

    File for second part

--- lab1_2.h
+++ lab1_2.h
@@ -1,53 +1,19 @@
-/* Заголовочный файл содержащий класс для lab1_2.cpp*/
-#include <string>
+/* Создание класса Работник для вывода статистики (Имени, его номера, и оклада)
+Если оклад превысит 50 000, то он недопустим*/
 
-class Students {
+class employee
+{
     public:
-        // Установка имени студента
-        void set_name(std::string student_name)
-        {
-            name = student_name;
-        }
-        // Получение имени студента
-        std::string get_name()
-        {
-            return name;
-        }
-        // Установка фамилии студента
-        void set_last_name(std::string student_last_name)
-        {
-            last_name = student_last_name;
-        }
-        // Получение фамилии студента
-        std::string get_last_name()
-        {
-            return last_name;
-        }
-        // Установка промежуточных оценок
-        void set_scores(int student_scores[])
-        {
-            for (int i = 0; i < 5; ++i) {
-                scores[i] = student_scores[i];
-            }
-        }
-        // Установка среднего балла
-        void set_average_ball(float ball)
-        {
-            average_ball = ball;
-        }
-        // Получение среднего балла
-        float get_average_ball()
-        {
-            return average_ball;
-        }
+        employee(char *, long, float);
+        ~employee(void);
+
+    void show_employee(void);
+    int change_salary(float);
+    long get_id(void);
 
     private:
-        // Промежуточные оценки
-        int scores[5];
-        // Средний балл
-        float average_ball;
-        // Имя
-        std::string name;
-        // Фамилия
-        std::string last_name;
+        char name [64] ;
+        long employee_id;
+        float salary;
 };
+
